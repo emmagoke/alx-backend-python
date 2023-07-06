204b9c0543106c5fbdc4a072e88e177c27171328
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-06 13:16:57 +0100

    Added question 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This script contains an annotated python script that takes
+a float a and a float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ This function is annotated to return a float. """
+    return a + b
