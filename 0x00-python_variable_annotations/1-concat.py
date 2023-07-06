96b8b49be2783fb351d194eec582cb0454f62d94
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-06 13:23:10 +0100

    Added question 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This script contains an annotated python script that takes
+takes a string str1 and a string str2 as arguments and
+returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This is an annotated function that returns a string."""
+    return str1 + str2
