1b2a255ca49c0a6b579f424f9fdb58b75f2c6d2b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-06 13:31:52 +0100

    Added question 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This script contains an annotated python script that take stakes
+takes a float n as argument and returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This is an annoted function that takes a float
+    and returns it floor (an int)
+    """
+    return math.floor(n)
