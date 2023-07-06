1ab484e070d5780e1206cde78e6be112132aeecb
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-06 13:50:13 +0100

    Added question 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This script contains an annotated python script that take stakes
+takes a float n as argument and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """ This an annotated function that converts a float to a string"""
+    return str(n)
