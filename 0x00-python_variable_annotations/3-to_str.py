28301eb794c8685c03e25cbb47df330dd5757bdc
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-06 13:52:23 +0100

    Added question 4

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This script contains an annotated python script that take stakes
+This script contains an annotated python function that take takes
 takes a float n as argument and returns the string representation of the float
 """
 
