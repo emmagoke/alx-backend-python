72995308d6ed3cba131cdb9218adc2551584654d
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-10 14:14:45 +0100

    Added task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+THis script contains an asynchronous function
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """ This function waits for a random delay between 0 and max_delay. """
+
+    delay = random.uniform(0, max_delay + 1)
+    await asyncio.sleep(delay)
+    return delay
