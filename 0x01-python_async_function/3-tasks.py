60b984deec292759f8d71a555881af4d4a87a0a0
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-10 15:50:18 +0100

    Added task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This script contains a function that creates an asynchronous task
+"""
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    This function takes an integer max_delay and returns a asyncio.Task
+    """
+
+    task = asyncio.create_task(wait_random(max_delay))
+
+    return task
