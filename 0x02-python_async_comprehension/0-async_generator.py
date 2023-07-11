b6305b1ae4d56c02f686e273f5bcceaae63583b9
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-11 12:48:53 +0100

    Added an aynchronous generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ This script contains an asynchronous generator
 """
 import asyncio
 import random
-from typing import Iterator
+from typing import Generator
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> Generator[float]:
     """
     The coroutine will loop 10 times,
     each time asynchronously wait 1 second,
