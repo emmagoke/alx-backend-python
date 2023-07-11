0ed1d54f2129cba5fcc7a9df40a3d58fe7bc813e
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-11 13:07:49 +0100

    Added an aynchronous function that generates an an async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """
 This script contain an asynchronous comprehension function.
 """
-from typing import Iterator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterator[float]:
+async def async_comprehension() -> List[float]:
     """
     The coroutine will collect 10 random numbers using an async
     comprehensing over async_generator,
