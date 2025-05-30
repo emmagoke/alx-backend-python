aad61a180d83b9c915d5223eaebb96eebfcdb91f
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-05-30 17:14:06 +0100

    feat: add the rest_framework app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # INSTALLED APPS
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
