4a9e68cbac3dfd2669878f4bbf1bead028b1b736
Emmanuel Olagoke <emma.goke05@gmail.com>
2025-06-08 23:14:40 +0100

    feat: added the filter and pagination default backend

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # INSTALLED APPS
     "rest_framework",
+    "rest_framework_simplejwt",
+    "django_filters",
 
     # LOCAL APPS
     'chats.apps.ChatsConfig',
@@ -140,6 +142,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    "PAGE_SIZE": "20",
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
 }
 
 # Simple JWT settings
